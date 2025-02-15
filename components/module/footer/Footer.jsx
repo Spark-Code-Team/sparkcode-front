3d7c858e1c86d68c7c99c2986abefa415817cadb
omid-develop <omidataee20041382@gmail.com>
2025-02-15 12:49:47 +0330

    create component footer

--- components/module/footer/Footer.jsx
+++ components/module/footer/Footer.jsx
@@ -1,10 +1,100 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = ()=>{
 
     return(
 
         <>
 
-            <div className="w-full lg:h-[338px] bg-[#12202F] text-white flex justify-center items-center text-[50px] mt-16">Footer</div>
+            <div className="w-full lg:h-[338px] bg-[#12202F] lg:mt-32 text-white">
+
+                <div className="lg:w-[93%] w-full m-auto flex flex-wrap">
+
+                    <div className="lg:w-1/4">
+
+                        <div className="lg:w-[95%] m-auto lg:mt-6">
+                            <Image src="/image/logo-footer.png" alt="" width={180} height={64}/>
+                        </div>
+
+                        <div className="lg:w-[95%] m-auto lg:mt-6">
+                            <p className="text-sm text-[#d4e1f2]">تیم نرم افزاری اسپارک کد مرجع برنامه نویسی انواع اپلیکیشن ، وب سایت و نرم افزارهای تخصصی</p>
+                        </div>
+
+                        <div className="lg:w-[95%] m-auto lg:mt-6 flex gap-x-3">
+
+                            <Image src="/image/insta-footer.png" alt="" width={20} height={20}/>
+
+                            <Image src="/image/linkedin-footer.png" alt="" width={20} height={20}/>
+
+                        </div>
+
+                    </div>
+
+                    <div className="lg:w-1/4">
+
+                        <div className="lg:w-[95%] m-auto lg:mt-6">
+
+                            <p className="font-bold">دسترسی مستقیم</p>
+
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">پروژه ها</Link>
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">دوره ها</Link>
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">مقاله ها</Link>
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">تماس با ما</Link>
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">درباره ما</Link>
+
+                        </div>
+
+                    </div>
+
+                    <div className="lg:w-1/4">
+
+                        <div className="lg:w-[95%] m-auto lg:mt-6">
+
+                            <p className="font-bold">خدمات ما</p>
+
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">نرم افزار مدیریت ارتباط با مشتری</Link>
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">قالب وردپرس</Link>
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">طراحی سایت</Link>
+                            <Link href="#" className="lg:mt-4 lg:block text-[15px] text-[#d4e1f2]">پشتیبانی</Link>
+
+                        </div>
+
+                    </div>
+
+                    <div className="lg:w-1/4">
+
+                        <div className="lg:w-[95%] m-auto lg:mt-6">
+
+                            <p className="font-bold">ارتباط با ما</p>
+
+                            <div className="lg:mt-4 lg:flex lg:items-center gap-x-2 text-[15px] text-[#d4e1f2]">
+                                <Image src="/image/email-footer.png" alt="" width={20} height={20}/>
+                                <p>Info@sparkcode-service.com</p>
+                            </div>
+
+                            <div className="lg:mt-4 lg:flex lg:items-center gap-x-2 text-[15px] text-[#d4e1f2]">
+                                <Image src="/image/phone-footer.png" alt="" width={20} height={20}/>
+                                <p>9961354854 98+ </p>
+                            </div>
+
+                            <div className="lg:mt-4 lg:flex lg:items-center gap-x-2 text-[15px] text-[#d4e1f2]">
+                                <Image src="/image/map-footer.png" alt="" width={20} height={20}/>
+                                <p>تهران، تهران، خیابان پاسداران، بوستان پنج</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    <div className="w-full lg:h-[1px] mt-10 bg-[#d4e1f2]"> </div>
+
+                    <div className="w-full m-auto text-center lg:mt-4 text-[#d4e1f2]">کلیه حقوق مادی و معنوی این سایت متعلق به اسپارک کد است.</div>
+
+                </div>
+
+            </div>
 
         </>
 
--- components/template/home/ContactUs.js
+++ components/template/home/ContactUs.js
@@ -6,37 +6,37 @@ const ContactUs = ()=>{
 
         <>
 
-            <div className="w-full lg:mt-24 mt-10 bg-red-500">
+            <div className="w-full lg:mt-24 mt-10 text-white relative">
 
-                <div className="w-full bg-orange-400 text-center lg:text-3xl text-xl bg-gradient-to-b from-[#4899EB] to-[#4899EB]/0 opacity-80 text-transparent bg-clip-text">About Spark Code</div>
-                <div className="w-full bg-orange-300 text-center lg:text-2xl text-xl lg:mt-1 text-white font-bold">درباره اسپارک کد</div>
+                <div className="w-full text-center lg:text-3xl text-xl bg-gradient-to-b from-[#4899EB] to-[#4899EB]/0 opacity-80 text-transparent bg-clip-text">About Spark Code</div>
+                <div className="w-full text-center lg:text-2xl text-xl lg:mt-1 text-white font-bold">درباره اسپارک کد</div>
 
-                <div className="lg:w-[85%] w-full flex m-auto mt-14 bg-lime-500">
+                <div className="glass-background lg:w-[85%] w-full flex m-auto mt-14 border-2 border-[#182128] rounded-3xl">
 
-                    <div className="lg:w-[60%] bg-blue-400 flex flex-wrap lg:relative custom-border-contact">
+                    <div className="lg:w-[65%] lg:pr-10 flex flex-wrap lg:relative custom-border-contact">
 
-                        <div className="lg:w-1/2 bg-green-400 lg:mt-10">
-                            <label className="w-full bg-pink-200 block">نام *</label>
-                            <input type="text" name="" className="lg:w-[296px] lg:h-[54px] pr-3 rounded-xl mt-4" placeholder="نام خود را وارد کنید"/>
+                        <div className="lg:w-1/2 lg:mt-10">
+                            <label className="w-full block">نام *</label>
+                            <input type="text" name="" className="lg:w-[296px] lg:h-[54px] pr-3 rounded-xl mt-4 bg-transparent border-2 border-[#182128] outline-none" placeholder="نام خود را وارد کنید"/>
                         </div>
 
-                        <div className="lg:w-1/2 bg-green-600 lg:mt-10">
-                            <label className="w-full bg-pink-200 block">نام خانوادگی *</label>
-                            <input type="text" name="" className="lg:w-[296px] lg:h-[54px] pr-3 rounded-xl mt-4" placeholder="نام خانوادگی خود را وارد کنید"/>
+                        <div className="lg:w-1/2 lg:mt-10">
+                            <label className="w-full block">نام خانوادگی *</label>
+                            <input type="text" name="" className="lg:w-[296px] lg:h-[54px] pr-3 rounded-xl mt-4 bg-transparent border-2 border-[#182128] outline-none" placeholder="نام خانوادگی خود را وارد کنید"/>
                         </div>
 
-                        <div className="lg:w-1/2 bg-green-400 lg:mt-10">
-                            <label className="w-full bg-pink-200 block">شماره تماس *</label>
-                            <input type="tel" name="" className="lg:w-[296px] lg:h-[54px] pl-3 rounded-xl mt-4 text-left" placeholder="98+"/>
+                        <div className="lg:w-1/2 lg:mt-10">
+                            <label className="w-full block">شماره تماس *</label>
+                            <input type="tel" name="" className="lg:w-[296px] lg:h-[54px] pl-3 rounded-xl mt-4 text-left bg-transparent border-2 border-[#182128] outline-none" placeholder="98+"/>
                         </div>
 
-                        <div className="lg:w-1/2 bg-green-600 lg:mt-10">
-                            <label className="w-full bg-pink-200 block">آدرس ایمیل *</label>
-                            <input type="email" name="" className="lg:w-[296px] lg:h-[54px] pr-3 rounded-xl mt-4" placeholder="omid@gmail.com"/>
+                        <div className="lg:w-1/2 lg:mt-10">
+                            <label className="w-full block">آدرس ایمیل *</label>
+                            <input type="email" name="" className="lg:w-[296px] lg:h-[54px] pr-3 rounded-xl mt-4 bg-transparent border-2 border-[#182128] outline-none" placeholder="omid@gmail.com"/>
                         </div>
 
-                        <div className="lg:w-full bg-green-600 lg:mt-10 flex flex-wrap justify-around">
-                            <label className="w-full bg-pink-200 block">موضوع درخواست خود را مشخص کنید</label>
+                        <div className="lg:w-full lg:mt-10 flex flex-wrap justify-around">
+                            <label className="w-full block">موضوع درخواست خود را مشخص کنید</label>
 
                             <label className="lg:mt-5">UI UX
                                 <input type="radio" className=""/>
@@ -57,47 +57,47 @@ const ContactUs = ()=>{
                         </div>
 
 
-                        <div className="lg:w-full bg-green-600 lg:mt-10">
+                        <div className="lg:w-full lg:mt-10">
 
-                            <label className="w-full bg-pink-200 block">درخواست خود را بنویسید *</label>
-                            <textarea className="lg:w-[94%] lg:mt-5 lg:pr-4 pt-3 rounded-xl" placeholder="متن پیام را وارد کنید"> </textarea>
+                            <label className="w-full block">درخواست خود را بنویسید *</label>
+                            <textarea className="lg:w-[94%] lg:mt-5 lg:pr-4 pt-3 rounded-xl bg-transparent border-2 border-[#182128] outline-none" placeholder="متن پیام را وارد کنید"> </textarea>
 
                         </div>
 
-                        <div className="lg:w-full bg-green-600 lg:mt-10 flex justify-end lg:pl-11">
+                        <div className="lg:w-full lg:mt-10 lg:mb-10 flex justify-end lg:pl-11">
                             <button className="lg:w-[118px] lg:h-[45px] bg-[#2e567d] text-white rounded-lg">ارسال</button>
                         </div>
 
                     </div>
 
-                    <div className="lg:w-[40%] bg-blue-600 pt-10 flex flex-wrap">
+                    <div className="lg:w-[35%] pt-4 flex flex-wrap">
 
-                        <div className="lg:w-[90%] bg-yellow-300 m-auto">
+                        <div className="lg:w-[90%] m-auto lg:pr-7">
 
-                            <div className="flex justify-end gap-x-2">
-                                <p>omidataei1382@gmail.com</p>
+                            <div className="flex gap-x-2 lg:mt-6">
                                 <Image src="/image/email.png" alt="" width={22} height={25}/>
+                                <p>omidataei1382@gmail.com</p>
                             </div>
 
-                            <div className="flex justify-end gap-x-2 lg:mt-4">
-                                <p>پاسداران - بوستان 5 - پلاک 84</p>
+                            <div className="flex gap-x-2 lg:mt-6">
                                 <Image src="/image/location.png" alt="" width={22} height={25}/>
+                                <p>پاسداران - بوستان 5 - پلاک 84</p>
                             </div>
 
                         </div>
 
 
-                        <div className="lg:w-[90%] bg-yellow-300 m-auto lg:mt-6 flex justify-around">
+                        <div className="lg:w-[90%] m-auto lg:mt-80 flex justify-around">
 
-                            <div className="lg:w-[48px] lg:h-[48px] bg-orange-500">
+                            <div className="lg:w-[48px] lg:h-[48px]">
                                 <Image src="/image/telegram.png" alt="" width={48} height={48}/>
                             </div>
 
-                            <div className="lg:w-[48px] lg:h-[48px] bg-orange-500">
+                            <div className="lg:w-[48px] lg:h-[48px]">
                                 <Image src="/image/linkedin.png" alt="" width={48} height={48}/>
                             </div>
 
-                            <div className="lg:w-[48px] lg:h-[48px] bg-orange-500">
+                            <div className="lg:w-[48px] lg:h-[48px]">
                                 <Image src="/image/instagram.png" alt="" width={48} height={48}/>
                             </div>
 
@@ -106,6 +106,10 @@ const ContactUs = ()=>{
                     </div>
 
                 </div>
+
+                <div className="lg:w-[132px] lg:h-[132px] rounded-full bg-[#2e567d] absolute left-40 top-14 -z-10"> </div>
+                <div className="lg:w-[162px] lg:h-[162px] rounded-full bg-[#2e567d] absolute left-80 top-40 -z-10"> </div>
+                <div className="lg:w-[250px] lg:h-[250px] rounded-full bg-[#2e567d] absolute left-0 -bottom-20 -z-10"> </div>
 
             </div>
 
