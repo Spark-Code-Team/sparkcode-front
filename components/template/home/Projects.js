b3cc3e34c98903f3dcc39488d66d694044ee7528
omid-develop <omidataee20041382@gmail.com>
2025-02-16 14:09:56 +0330

    edit

--- components/template/home/Projects.js
+++ components/template/home/Projects.js
@@ -15,20 +15,20 @@ const Projects = ()=>{
 
                 <div className="lg:w-[85%] w-full lg:flex m-auto mt-14 lg:gap-x-5">
 
-                    <div className="lg:w-1/3 w-[75%] lg:m-0 m-auto lg:mt-0 mt-4 pb-5 border-2 border-[#182128] rounded-3xl text-white hover:animate-textGlow">
+                    <div className="lg:w-1/3 w-[75%] lg:m-0 m-auto lg:mt-0 mt-4 pb-5 border-2 border-[#182128] rounded-3xl text-white hover:animate-textGlow flex flex-col">
 
                         <div className="lg:w-full lg:h-[200px] w-full h-[200px] bg-[#414141]/[0.27] rounded-t-3xl">
                             {/*  image  */}
                         </div>
 
-                        <div className="w-full mt-8 flex items-center gap-x-3 text-xl font-bold pr-5">
+                        <div className="w-full mt-8 min-h-[50px] flex items-center gap-x-3 text-xl font-bold pr-5 flex-grow">
 
                             <Image src="/image/logo-evaam.png" alt="" width={47} height={28}/>
                             <p>ایوام</p>
 
                         </div>
 
-                        <div className="w-full mt-8 pr-5 pl-5 text-sm">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید. در حال حاضر هر ۶ماه یک‌بار، تعداد کاربران و درخواست‌ها به‌سمت نشان دو برابر می‌شود.</div>
+                        <div className="w-full mt-8 pr-5 pl-5 text-sm flex-grow">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید. در حال حاضر هر ۶ماه یک‌بار، تعداد کاربران و درخواست‌ها به‌سمت نشان دو برابر می‌شود.</div>
 
                         <div className="w-full mt-8 flex justify-end items-center text-red-700 pl-4">
                             <Link href="#" className="flex items-center">مشاهده وب سایت <ArrowLeftRed/></Link>
@@ -38,19 +38,19 @@ const Projects = ()=>{
 
 
 
-                    <div className="lg:w-1/3 w-[75%] lg:m-0 m-auto lg:mt-0 mt-4 pb-5 border-2 border-[#182128] rounded-3xl text-white hover:animate-textGlow">
+                    <div className="lg:w-1/3 w-[75%] lg:m-0 m-auto lg:mt-0 mt-4 pb-5 border-2 border-[#182128] rounded-3xl text-white hover:animate-textGlow flex flex-col">
 
                         <div className="lg:w-full lg:h-[200px] w-full h-[200px] bg-[#414141]/[0.27] rounded-t-3xl">
                             {/*  image  */}
                         </div>
 
-                        <div className="w-full mt-8 flex items-center gap-x-3 text-xl font-bold pr-5">
+                        <div className="w-full min-h-[50px] mt-8 flex items-center gap-x-3 text-xl font-bold pr-5">
 
                             <p>لونا</p>
 
                         </div>
 
-                        <div className="w-full mt-8 pr-5 pl-5 text-sm">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید. در حال حاضر هر ۶ماه یک‌بار، تعداد کاربران و درخواست‌ها به‌سمت نشان دو برابر می‌شود.</div>
+                        <div className="w-full mt-8 pr-5 pl-5 text-sm flex-grow">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید. در حال حاضر هر ۶ماه یک‌بار، تعداد کاربران و درخواست‌ها به‌سمت نشان دو برابر می‌شود.</div>
 
                         <div className="w-full mt-8 flex justify-end items-center text-red-700 pl-4">
                             <Link href="#" className="flex items-center">مشاهده وب سایت <ArrowLeftRed/></Link>
@@ -60,20 +60,20 @@ const Projects = ()=>{
 
 
 
-                    <div className="lg:w-1/3 w-[75%] lg:m-0 m-auto lg:mt-0 mt-4 pb-5 border-2 border-[#182128] rounded-3xl text-white hover:animate-textGlow">
+                    <div className="lg:w-1/3 w-[75%] lg:m-0 m-auto lg:mt-0 mt-4 pb-5 border-2 border-[#182128] rounded-3xl text-white hover:animate-textGlow flex flex-col">
 
                         <div className="lg:w-full lg:h-[200px] w-full h-[200px] bg-[#414141]/[0.27] rounded-t-3xl">
                             {/*  image  */}
                         </div>
 
-                        <div className="w-full mt-8 flex items-center gap-x-3 text-xl font-bold pr-5">
+                        <div className="w-full min-h-[50px] mt-8 flex items-center gap-x-3 text-xl font-bold pr-5 flex-grow">
 
                             <Image src="/image/logo-matin.png" alt="" width={47} height={28}/>
                             <p>متین خودرو</p>
 
                         </div>
 
-                        <div className="w-full mt-8 pr-5 pl-5 text-sm">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید. در حال حاضر هر ۶ماه یک‌بار، تعداد کاربران و درخواست‌ها به‌سمت نشان دو برابر می‌شود.</div>
+                        <div className="w-full mt-8 pr-5 pl-5 text-sm flex-grow ">مقیاس‌پذیری زیرساخت‌های ابری کمک می‌کند تا به تناسب رشد یک کسب‌وکار، دغدغه‌های زیرساختی نداشته باشید. در حال حاضر هر ۶ماه یک‌بار، تعداد کاربران و درخواست‌ها به‌سمت نشان دو برابر می‌شود.</div>
 
                         <div className="w-full mt-8 flex justify-end items-center text-red-700 pl-4">
                             <Link href="#" className="flex items-center">مشاهده وب سایت <ArrowLeftRed/></Link>
