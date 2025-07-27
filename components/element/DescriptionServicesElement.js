448f22842c49f1a1723f6b5246aceb02f60166d9
Alireza Salman <itcsalman82@gmail.com>
2025-07-27 06:37:52 -0700

    dokme

--- components/element/DescriptionServicesElement.js
+++ components/element/DescriptionServicesElement.js
@@ -12,14 +12,14 @@ const DescriptionServicesElement = () => {
 
                 <div className={`lg:w-[85%] w-full m-auto lg:mt-14 p-7 lg:p-0 flex flex-col lg:flex-row `}>
 
-                    <div className="lg:w-1/2 m-auto">
+                    <div className="lg:w-1/2 m-auto p-">
                         <p className="text-xl text-white">توسعه وب سایت</p>
                         <div className="w-full text-white lg:m-auto lg:mt-7">
 
                         توسعه وب‌سایت فراتر از پیاده‌سازی صفحات و نوشتن کد است؛ ما معماری نرم‌افزار را بر اساس اهداف تجاری، نیازهای کاربران و آینده‌نگری طراحی میکنیم.  از انتخاب دقیق فناوری‌های فرانت‌اند و بک‌اند گرفته تا طراحی پایگاه‌داده و یکپارچه‌سازی با سرویس‌های خارجی، هر بخش با دقت مهندسی و نگاه سیستماتیک ساخته می‌شود. ما فرآیند توسعه را با استفاده از متدولوژی‌های مدرن مانند Agile و DevOps پیش می‌بریم تا تحویل سریع، کنترل کیفیت پیوسته و قابلیت توسعه در آینده تضمین شود. هدف ما ساختن وب‌سایت‌هایی است که نه‌فقط کار کنند، بلکه قابل اعتماد، توسعه‌پذیر و آماده برای رشد باشند.
 
                             </div>
-                        <Link href="/?showComponent=true#bottom" className="bg-[#2E567D] lg:w-[153px] lg:h-[40px] lg:flex lg:justify-center lg:items-center lg:mt-7 rounded-md lg:gap-x-3 text-white">
+                        <Link href="/?showComponent=true#bottom" className="bg-[#2E567D] w-fit flex gap-3 lg:w-[153px] lg:h-[40px] lg:flex lg:justify-center lg:items-center lg:mt-7 rounded-md lg:gap-x-3 text-white">
                             تماس با ما<ArrowLeftService />
                         </Link>
                     </div>
